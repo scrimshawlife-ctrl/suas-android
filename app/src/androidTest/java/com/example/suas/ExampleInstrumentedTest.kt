9678657ba3cee2285b651db804a0facd2481b591
Daniel <scrimshawlife@AAL-mainnet.localdomain>
2026-09-04 13:03:58 -0700

    test: use Compose test rule v2

--- app/src/androidTest/java/com/example/suas/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/suas/ExampleInstrumentedTest.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.assertTextContains
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
